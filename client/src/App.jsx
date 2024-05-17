578153f7e82e64508f53d7df2151fa850af42a50
Tquillity <mikael.sundh@gmail.com>
2024-05-17 14:15:15 +0200

    Initial folder- and filestructure plan

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Vote Ledger</h1>
+
+      <p>Vote Ledger is a decentralized voting application that.</p>
+      <p>uses the StateLayer blockchain to store votes.</p>
+    </div>
+  );
+}
+
+export default App;
