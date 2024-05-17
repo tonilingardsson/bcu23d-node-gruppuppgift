4f3bc08ced0d076cd6a46e2b8ea5f8c3201e38f4
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-05-17 22:10:20 +0200

    Updated server.mjs file.

--- server/server.mjs
+++ server/server.mjs
@@ -1,7 +1,24 @@
 import express from 'express';
+import { PORT } from './startup.mjs';
+
+import errorHandler from './middlewares/errorHandler.mjs';
+import logHandler from './middlewares/logHandler.mjs';
+
+import blockchainRoutes from './routes/blockchainRoutes.mjs';
+import membersRoutes from './routes/membersRoutes.mjs';
+
+import resourceNotFound from './utils/resourceNotFound.mjs';
 
 const app = express();
-
 app.use(express.json());
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+// app.use(logHandler);
+
+app.use('/api/v1/blockchain', blockchainRoutes);
+// app.use('/api/v1/members', membersRoutes);
+
+app.all('*', resourceNotFound);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
