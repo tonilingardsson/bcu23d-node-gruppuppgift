54c6433f879d9c064747b85a9a8746785da6a14a
Yellik <yelenakensborn@gmail.com>
2024-05-23 16:26:44 +0200

    added a transactionView component

--- server/src/routes/router.mjs
+++ server/src/routes/router.mjs
@@ -1,4 +1,5 @@
 import express from 'express'
+import presentTransactions from '../controllers/presentTransactions.mjs';
 
 const router = express.Router();
 router.route('/api/v1/transactions').get(presentTransactions);
