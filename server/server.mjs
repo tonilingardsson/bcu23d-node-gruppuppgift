13d670bea6517d8c6ff6d8e710b130ddb25a990b
Yellik <yelenakensborn@gmail.com>
2024-05-23 15:49:24 +0200

    commented out non existing modules

--- server/server.mjs
+++ server/server.mjs
@@ -1,24 +1,25 @@
 import express from 'express';
-import { PORT } from './startup.mjs';
+//import { PORT } from './startup.mjs';
 
-import errorHandler from './middlewares/errorHandler.mjs';
-import logHandler from './middlewares/logHandler.mjs';
+//import errorHandler from './middlewares/errorHandler.mjs';
+//import logHandler from './middlewares/logHandler.mjs';
 
-import blockchainRoutes from './routes/blockchainRoutes.mjs';
-import membersRoutes from './routes/membersRoutes.mjs';
+//import blockchainRoutes from './routes/blockchainRoutes.mjs';
+//import membersRoutes from './routes/membersRoutes.mjs';
 
-import resourceNotFound from './utils/resourceNotFound.mjs';
+//import resourceNotFound from './utils/resourceNotFound.mjs';
 
 const app = express();
 app.use(express.json());
 
+const PORT = 3000;
 // app.use(logHandler);
 
-app.use('/api/v1/blockchain', blockchainRoutes);
+//app.use('/api/v1/blockchain', blockchainRoutes);
 // app.use('/api/v1/members', membersRoutes);
 
-app.all('*', resourceNotFound);
+//app.all('*', resourceNotFound);
 
-app.use(errorHandler);
+//app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
