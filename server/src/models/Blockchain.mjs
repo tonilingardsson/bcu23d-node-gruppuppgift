42f40b82283b5075beaea2343721a5d3a0106fcf
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2024-05-25 00:18:51 +0200

    Add createBlock in Blockchain model

--- server/src/models/Blockchain.mjs
+++ server/src/models/Blockchain.mjs
@@ -1,7 +1,61 @@
+import { createHash } from "../utilities/crypto-lib.mjs";
+import Block from "./Block.mjs";
+import Transaction from "./Transaction.mjs";
+
 export default class Blockchain {
   constructor() {
     this.chain = [];
     this.memberNodes = [];
     this.pendingTransactions = [];
+
+    this.nodeUrl = process.argv[3];
   }
-}
+
+  createBlock(
+    timestamp,
+    prevBlockHash,
+    currBlockHash,
+    data,
+    nonce,
+    difficulty
+  ) {
+    const block = new Block(
+      timestamp,
+      this.chain.length + 1,
+      prevBlockHash,
+      currBlockHash,
+      data,
+      nonce,
+      difficulty
+    );
+    this.pendingTransactions = [];
+    this.chain.push(block);
+
+    return block;
+  }
+
+  createTransaction(amount, sender, recipient) {
+    return new Transaction(amount, sender, recipient);
+  }
+
+  addTransaction(transaction) {
+    this.pendingTransactions.push(transaction);
+    return this.getLastBlock().blockIndex + 1;
+  }
+
+  getLastBlock() {
+    return this.chain.at(-1);
+  }
+
+  hashBlock(timestamp, prevBlockHash, currBlockHash, nonce, difficulty) {
+    const stringToHash = timestamp.toString() +
+      timestamp.toString() +
+      prevBlockHash +
+      JSON.stringify(currBlockHash) +
+      nonce +
+      difficulty;
+    const hash = createHash()
+  }
+ }
+
+
