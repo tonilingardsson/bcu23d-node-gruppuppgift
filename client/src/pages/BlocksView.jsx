4dc22f23e819eaf4a144329553c84a26e1b3711a
Yellik <yelenakensborn@gmail.com>
2024-05-27 18:17:19 +0200

    implemented useeffect with Axios

--- client/src/pages/BlocksView.jsx
+++ client/src/pages/BlocksView.jsx
@@ -3,6 +3,25 @@ import { useState, useEffect } from "react";
 import axios from axioss
 
 const BlockView = () => {
+    const[blocks, setBlocks] = useState([]);
+
+    useEffect(() => {
+        const fetchBlocks = async() => {
+            try{
+                const response = await axios.get('http://localhost:3000/api/v1/blockchain');
+                
+                const blocksData = response.data.data.chain
+    
+                setBlocks(blocksData)
+             
+              
+            }catch(error)  {
+                    console.error('Error fetching data:', error);
+                };
+            
+            }
+            fetchBlocks()
+        }, []);
     return(
         <>
         </>
