d7bd4318cfce066e999be5ef6921d98e502bfaaf
Yellik <yelenakensborn@gmail.com>
2024-05-27 18:18:10 +0200

    BlocksView

--- client/src/pages/BlocksView.jsx
+++ client/src/pages/BlocksView.jsx
@@ -22,10 +22,26 @@ const BlockView = () => {
             }
             fetchBlocks()
         }, []);
-    return(
-        <>
-        </>
-    )
+        return (
+            <div className="Blocks">
+                <h1>Blockchain Data</h1>
+                {blocks.length > 0 ? (
+                    blocks.map((block, index) => (
+                        <div key={index} className="block">
+                            <h2>Block {block.blockIndex}</h2>
+                            <p><strong>Timestamp:</strong> {block.timestamp}</p>
+                            <p><strong>Previous Block Hash:</strong> {block.prevBlockHash}</p>
+                            <p><strong>Current Block Hash:</strong> {block.currBlockHash}</p>
+                            <p><strong>Nonce:</strong> {block.nonce}</p>
+                            <p><strong>Data:</strong> {block.data.length > 0 ? JSON.stringify(block.data) : 'No data'}</p>
+                        </div>
+                    ))
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </div>
+        );
+    
 }
 
 export default BlockView
