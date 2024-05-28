775954d55757782c8f6a3a892550c29ad688d98b
Tquillity <mikael.sundh@gmail.com>
2024-05-28 08:18:35 +0200

    Adding CORS Dependency.
    1. Cleaning up Server from commented out unused code.
    2. added app use for cors. all open.

--- server/server.mjs
+++ server/server.mjs
@@ -1,42 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import transactionRoutes from './src/routes/transactionRoutes.mjs'
-
-// import router from './src/routes/router.mjs'
-// import presentTransactions from './src/controllers/presentTransactions.mjs';
-
-//import errorHandler from './middlewares/errorHandler.mjs';
-//import logHandler from './middlewares/logHandler.mjs';
-
-
 import blockchainRoutes from '../server/src/routes/blockchainRoutes.mjs';
 import memberRoutes from './src/routes/memberRoutes.mjs';
 
-
-//import resourceNotFound from './utils/resourceNotFound.mjs';
-
 const app = express();
 app.use(express.json());
+app.use(cors()); // Enable CORS for all requests
 
 const PORT = process.argv[2];
-
-// app.use('/api/v1/transactions', router);
-
-// app.use(logHandler);
 
 app.use('/api/v1/blockchain', blockchainRoutes);
 app.use('/api/v1/members', memberRoutes);
 app.use('/api/v1/transactions', transactionRoutes);
-
-
-// router.route('/api/v1/transactions').get(presentTransactions)
-
-//app.all('*', resourceNotFound);
-//app.use(errorHandler);
-// app.use(router)
-
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html')); // Fallback to index.html
-// });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
