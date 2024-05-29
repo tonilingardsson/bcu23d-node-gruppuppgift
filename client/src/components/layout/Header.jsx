a5275a0d10571e47e8bd398c6fc13b7187c402f1
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-05-29 19:25:37 +0200

    Updating header.

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../../assets/vl-logo.webp';
-import ThemeToggle from './themes/ThemeToggle';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/vl-logo.webp";
+import ThemeToggle from "./themes/ThemeToggle";
 
 export const Header = () => {
   return (
     <header>
-      <div className='header-container'>
-        <img src={logo} alt='VoteLedger Logo' className='logo' />
+      <div className="header-container">
+        <img src={logo} alt="VoteLedger Logo" className="logo" />
         <nav>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/vote">Vote</Link></li>
-            <li><Link to="/transaction-form">TransactionForm</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
-            <li><Link to="/blockchain">Blockchain</Link></li> {/* Add this link */}
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            {/* <li><Link to="/vote">Vote</Link></li> */}
+            <li>
+              <Link to="/transaction-form">TransactionForm</Link>
+            </li>
+            {/* <li><Link to="/login">Login</Link></li> */}
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/blockchain">Blockchain</Link>
+            </li>{" "}
+            {/* Add this link */}
           </ul>
         </nav>
         <ThemeToggle />
