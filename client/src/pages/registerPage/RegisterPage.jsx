ba7042a35b7e069a941e9fda73c3955e87a6e0bf
Stefan Lahtinen <stefan.lahtinen@gmail.com>
2024-05-29 18:14:32 +0200

    add function to show registered nodes

--- client/src/pages/registerPage/RegisterPage.jsx
+++ client/src/pages/registerPage/RegisterPage.jsx
@@ -5,6 +5,8 @@ const RegisterPage = () => {
     const [nodeUrl, setNodeUrl] = useState('http://localhost:3002');
     const [port, setPort] = useState('http://localhost:3001');
     const [message, setMessage] = useState('');
+    const [memberNodes, setMemberNodes] = useState('');
+    const [show, setShow] = useState(false);
 
 
     const handleSubmit = async (event) => {
@@ -39,28 +41,59 @@ const RegisterPage = () => {
         }
     };
 
+    const showMemberNodes = async (event) => {
+        event.preventDefault();
+
+        try {
+            const blockchainResponse = await fetch('http://localhost:3001/api/v1/blockchain');
+            const blockchain = await blockchainResponse.json();
+
+            setMemberNodes(blockchain.data.memberNodes);
+            setShow(true);
+        } catch (error) {
+            setMessage(`Error: ${error.message}`);
+        }
+
+    }
+
   return (
     <div>
         <h1>Register your node</h1>
         <p>(remember to have your nodes up and running :p)</p>
         <form onSubmit={handleSubmit}>
-                    <InputField
-                        label="Register node"
-                        type="text"
-                        value={nodeUrl}
-                        onChange={(e) => setNodeUrl(e.target.value)}
-                        required
-                    />
-                    <InputField
-                        label="on port"
-                        type="text"
-                        value={port}
-                        onChange={(e) => setPort(e.target.value)}
-                        required
-                    />
-                    <button type="submit">Register</button>
-                </form>
-                {message && <p>{message}</p>}
+            <InputField
+                label="Register node"
+                type="text"
+                value={nodeUrl}
+                onChange={(e) => setNodeUrl(e.target.value)}
+                required
+            />
+            <InputField
+                label="on port"
+                type="text"
+                value={port}
+                onChange={(e) => setPort(e.target.value)}
+                required
+            />
+            <button type="submit">Register</button>
+        </form>
+        {message && <p>{message}</p>}
+
+        <br />
+        <h1>Show registered nodes</h1>
+        <button onClick={showMemberNodes}>Show</button>
+
+        {show && (
+            memberNodes.length > 0 ? (
+                <ul>
+                    {memberNodes.map((node, index) => (
+                        <li key={index}>{node}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No nodes are registered yet.</p>
+            )
+        )}
     </div>
   )
 }
